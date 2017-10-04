52db3e1505682e09f524d6dc6439dfcf341f2df5
Scott Linenberger <scott.linenberger@gmail.com>
2017-10-04 15:46:00 -0500

    Add Service example

---
+++ src/js/components/HasService.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import SampleSvc from '../services/SampleSvc';
+
+class HasService extends React.Component {
+  constructor(props) {
+    super(props);
+    /* init component */
+    this.state = {
+      serviceValue: SampleSvc.retrieveData(),
+    };
+  }
+
+  incrementCount1() {
+    console.log('Incrementing Count 1...');
+    SampleSvc.incrementCount1();
+    this.setState({
+      serviceValue: SampleSvc.retrieveData(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Has Service</h2>
+        <p>Service Count: {this.state.serviceValue.count1}</p>
+        <button onClick={(e) =>{this.incrementCount1(e)}}>Increment Count</button>
+      </div>
+    );
+  }
+}
+
+module.exports = HasService;
---
+++ src/js/services/SampleSvc.js
@@ -0,0 +1,27 @@
+
+class SampleSvc {
+  constructor() {
+    console.log('Constructing SampleSvc...');
+    
+    /* init counts */
+    this.count1 = 0;
+    this.count2 = 0;
+    this.count3 = 0;
+  }
+
+  incrementCount1() {
+    this.count1 += 1;
+  }
+
+  retrieveData() {
+    const {count1 } = this;
+    const data = {
+      count1,
+    };
+
+    console.log('Sending data: %O', data);
+    return data;
+  }
+}
+
+module.exports = new SampleSvc();
